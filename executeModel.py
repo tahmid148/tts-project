7566285d157b1eb8bf8b6a6be9529f6ebbe91a95
tahmid <tahmidrahman2812@outlook.com>
2023-09-01 18:59:04 +0100

    model used for generation is now Jenny

--- executeModel.py
+++ executeModel.py
@@ -8,26 +8,34 @@ path = "/home/tahmid/.local/lib/python3.10/site-packages/TTS/.models.json"
 # Initialise Model Manager
 model_manager = ModelManager(path)
 
+print("Downloading Models...")
 # Download TTS model and its associated vocoder
-model_name = "tts_models/en/ljspeech/glow-tts"
+model_name = "tts_models/en/jenny/jenny"
 model_path, config_path, model_item = model_manager.download_model(model_name)
-vocoder_name = model_item["default_vocoder"]
-voc_path, voc_config_path, _ = model_manager.download_model(vocoder_name)
+# vocoder_name = "vocoder_models/en/ljspeech/univnet"
+# voc_path, voc_config_path, _ = model_manager.download_model(vocoder_name)
 
 
+print("Creating Synthesizer...")
 # Create a Synthesizer instance
 syn = Synthesizer(
     tts_checkpoint=model_path,
     tts_config_path=config_path,
-    vocoder_checkpoint=voc_path,
-    vocoder_config=voc_config_path
+    # vocoder_checkpoint=voc_path,
+    # vocoder_config=voc_config_path
 )
 
 # Text to be converted into speech
-text = "I am a text readed by a computer"
-
+text_input_path = "input.txt"
+with open(text_input_path, "r", encoding="utf-8") as file:
+    text = file.read()
+    
+print("Converting text...")
 # Synthesize speech from text
 outputs = syn.tts(text)
 
+print("Saving output file...")
 # Save the generated speech as an audio file
 syn.save_wav(outputs, "output/audio.wav")
+
+print("Done")
